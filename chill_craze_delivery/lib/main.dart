3fb3b18cdc5cb48a4b093e57c3cd57068566412f
Alex-Benites <136673432+Alex-Benites@users.noreply.github.com>
2024-05-10 18:26:32 -0500

    Conexion a la base de datos lista

--- chill_craze_delivery/lib/main.dart
+++ chill_craze_delivery/lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:chill_craze_delivery/app.dart';
-import 'package:chill_craze_delivery/app_view.dart';
+//import 'package:chill_craze_delivery/app_view.dart';
 import 'package:chill_craze_delivery/simple_bloc_observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey:
+          "AIzaSyCBH2eVKW4E1h2DK0_fpYNS4TvC-8PN7r4", // paste your api key here
+      appId:
+          "1:941457330996:android:926bafa87c4580cb4e8b61", //paste your app id here
+      messagingSenderId: "941457330996", //paste your messagingSenderId here
+      projectId: "chillcraze2", //paste your project id here
+    ),
+  );
   Bloc.observer = SimpleBlocObserver();
   runApp(MyApp(FirebaseUserRepo()));
 }
