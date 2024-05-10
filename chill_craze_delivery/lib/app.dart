78ab6243fce4da3692b9418976c78f84a616cf78
Alex-Benites <136673432+Alex-Benites@users.noreply.github.com>
2024-05-10 15:51:32 -0500

    Conexion a la firebase

--- chill_craze_delivery/lib/app.dart
+++ chill_craze_delivery/lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
 
+import 'app_view.dart';
 import 'blocs/bloc/authentication_bloc.dart';
 
 class MyApp extends StatelessWidget {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       create: (context) => AuthenticationBloc(
         userRepository : userRepository 
       ),
-      child: MyAppView(),
+      child: const MyAppView(),
     );
   }
 }
---
+++ frozen_delights/android/app/src/main/kotlin/com/example/frozen_delights/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.frozen_delights
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
